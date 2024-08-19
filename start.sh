c19851134ebe0250cd3449a3fa14315545158536
stdEndian <010flower010@gmail.com>
2024-08-19 17:06:53 +0500

    Start QEMU

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Change OVFM directory if .fd file has different location
+
+ovmfDir=/usr/share/edk2/x64/OVMF.fd  # Replace with the path of your OVMF.fd 
+imageFile=image.img                  # Replace with the name of your disk image
 
 
-qemu-system-x86_64 -cpu qemu64 -bios /usr/share/edk2/x64/OVMF.fd -drive file=image.img,if=ide,format=raw
+qemu-system-x86_64 -cpu qemu64 -bios $ovmfDir -drive file=$imageFile,if=ide,format=raw
